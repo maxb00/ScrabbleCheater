be012c076b6291801785ce372e2520514dc5e6f8
maxb00 <maxbar00@yahoo.com>
2015-12-14 14:25:56 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import argparse
+scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+          "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+          "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+          "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+          "x": 8, "z": 10}
+words = open("sowpods.txt")
+
+parser = argparse.ArgumentParser(description='Help you win scrabble and Words with friends, '
+                                             'and maybe even get you that english major!')
+parser.add_argument("letters", type=str, help='Letters in your dock')
+args = parser.parse_args()
+print(args.letters)
+
